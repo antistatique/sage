c3b166e69d046ae3f8946b64c4704d8a3f87fb61
knuch <knuchelf@gmail.com>
2018-11-05 08:39:07 +0100

    extend cpt example

--- app/cpt/example.php
+++ app/cpt/example.php
@@ -16,5 +16,39 @@ function create_example() {
   register_post_type( 'example',
     $args
     );
+
+
+  register_taxonomy(
+    'example_category',
+    'example',
+    array(
+      'label' => __( 'Exemple de taxonomie', 'lang' ),
+      'public' => true,
+      'rewrite'     => ['slug' => 'example/slug'],
+      'hierarchical' => true,
+    )
+  );
 }
 add_action( 'init', 'create_example' );
+
+/**
+ * Hide default taxonomy metabox
+ */
+add_action( 'admin_menu', function () {
+  $cpt = 'example';
+  $taxonomy = 'example_category';
+	remove_meta_box( 'tagsdiv-'.$taxonomy, $cpt, 'side' );
+} );
+
+/**
+ * Alter default ordering using ACF
+ */
+add_action(
+	'pre_get_posts',
+	function ($query){
+		if(is_archive() && is_post_type_archive( 'example' )):
+				$query->set( 'meta_key', 'example_acf_slug' );
+				$query->set( 'orderby', ['meta_value' => 'DESC'] );
+		endif;
+	}
+);
