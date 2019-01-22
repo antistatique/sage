2ee9ced5cfc5887969525c0846b1a62dd2e3ec14
knuch <knuchelf@gmail.com>
2019-01-22 10:22:03 +0100

    add image without crop

--- app/image_sizes.php
+++ app/image_sizes.php
@@ -4,12 +4,21 @@
      */
 
 CONST IMAGES_SIZES = [
-/*     1 => ['image_1600px', 1600, 1067],
-    2 => ['image_1600px_retina', 3200, 2134],
-    3 => ['image_1140px', 1140, 760],
-    4 => ['image_1140px_retina', 2280, 1521] */
+/*  ['image_1600px', 1600, 1067],
+    ['image_1600px_retina', 3200, 2134],
+    ['image_1140px', 1140, 760],
+    ['image_1140px_retina', 2280, 1521] */
 ];
 
 foreach (IMAGES_SIZES as $image) {
     add_image_size($image[0], $image[1], $image[2], ['center', 'top']);
+}
+
+CONST IMAGES_SIZES_WITHOUT_CROPING = [
+    // Naming convention for the images sizes : ratio_width_size.
+    ['', '', ''],
+];
+
+foreach (IMAGES_SIZES_WITHOUT_CROPING as $image) {
+    add_image_size($image[0], $image[1], $image[2], false);
 }
